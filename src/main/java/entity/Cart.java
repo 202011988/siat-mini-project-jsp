5bcd18f33734661d642238392a8b14dcf55379dc
gntodtndls156 <gntodtndls156@gmail.com>
2024-05-23 12:05:15 +0900

    before merge

--- src/main/java/entity/Cart.java
+++ src/main/java/entity/Cart.java
@@ -7,23 +7,32 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cart {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;	// 번호
 
-	@ManyToOne
-	@JoinColumn(nullable = false)
-	private Product product;	// 상품 번호
-	
-	@ManyToOne
-	@JoinColumn(nullable = false)
-	private User user;	// 유저 번호
-	
-	@Column()
-	private Integer quantity = 0;		// 상품 수량
-	@Column()
-	private Integer price = 0;	// 상품 가격
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; // 번호
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Product product; // 상품 번호
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private User user; // 유저 번호
+
+    @Column()
+    private Integer quantity = 0; // 상품 수량
+    @Column()
+    private Integer price = 0; // 상품 가격
 }
