fe3d5d99907650e33ddb017f9ee58a7ebc3331ff
ChangChang <bbmini96@hanmail.net>
2024-05-23 14:18:01 +0900

    Product에 description annotation을 TEXT로 수정

--- src/main/java/test/shoppingProductTest.java
+++ src/main/java/test/shoppingProductTest.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+
 public class shoppingProductTest {
 
 	public static void main(String[] args) {
@@ -14,20 +15,8 @@ public class shoppingProductTest {
 		
 		tx.begin();
 		try {
-			
-			// Album : insert
-//			Album album1 = new Album("album1 : name", 1000, "album1 : artist");
-//			em.persist(album1);
-//			
-			// Film : insert
-//			Film film1 = new Film("film1 : name", 1100, "film1 : director", "film1 : actor");
-//			em.persist(film1);
 
-			// Album : select
-
-//			Album foundAlbum1 = em.find(Album.class, 1L);
-//			System.out.println(foundAlbum1);
-			
+	
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
