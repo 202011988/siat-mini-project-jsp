256cbd9b49f9356618c8030db14706bbcc6c4b64
Donggyu <dlehdrb5509@naver.com>
2024-05-26 08:16:40 +0900

    logout 구현, main path 규칙 추가

---
+++ src/main/java/controller/account/LogoutController.java
@@ -0,0 +1,27 @@
+package controller.account;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(value = "/logout.do")
+public class LogoutController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+
+        if(session != null) {
+            session.invalidate();
+            session = null;
+
+        }
+
+        resp.sendRedirect("/");
+        return;
+
+    }
+}
